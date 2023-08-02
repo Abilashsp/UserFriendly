cab3b13ddc1b7306217a88d77cf562e28b783e94
Abilashsp <139426960+Abilashsp@users.noreply.github.com>
2023-08-02 11:43:17 +0530

    Centerbar.js

--- src/Centerbar.js
+++ src/Centerbar.js
@@ -2,29 +2,30 @@ import React from "react";
 import DraggableList from "react-draggable-list";
 
 function Centerbar({ values, handelTextButton }) {
- 
+  // console.log("values", values);
+
+  const ItemTempalte = ({ item, ...rest }) => {
+    console.log("id",item.id);
+    // TODO : Index is not passed, so have the id in the values object
+    return (
+      <div
+        key={item.id}
+        className={`text-lg text-white cursor-pointer ${
+          item.bold ? "font-bold" : ""
+        } ${item.underLine ? "underline" : ""}
+    ${item.italics ? "italic" : ""}
+    ${item.straightthrough ? "line-through " : ""}`}
+        onClick={() => handelTextButton(item.id)}
+      >
+        {item.value}
+        {/* {console.log(item.bold)} */}
+      </div>
+    );
+  };
   return (
     <div className="h-auto max-w-full mt-1">
-      <div className="h-full w-92 mx-auto bg-black rounded-xl">
-        <DraggableList
-          list={values}
-          itemKey="value" 
-          template={(value, index) => (
-            <div
-              key={index}
-              className={`text-lg text-white cursor-pointer ${
-                value.bold ? "font-bold" : ""
-              } ${value.underLine ? "underline" : ""}
-              ${value.italics ? "italic" : ""}
-              ${value.straightthrough ? "line-through " : ""}`}
-              onClick={() => handelTextButton(index)}
-            >
-              {value.value}
-              {console.log(value.bold)}
-            </div>
-          )}
-          
-        />
+      <div className="h-full mx-auto bg-black w-92 rounded-xl">
+        <DraggableList list={values} itemKey="value" template={ItemTempalte} />
       </div>
     </div>
   );
